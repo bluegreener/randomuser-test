d25da650dfb91481a832d8c99725559abc52a422
bluegreener <25910044+bluegreener@users.noreply.github.com>
2023-04-06 15:00:55 -0700

    add comments

--- tests/api/include-exclude.spec.ts
+++ tests/api/include-exclude.spec.ts
@@ -3,8 +3,10 @@ import { inclExclOptions } from '../../util/constants';
 
 test('should exclude the correct fields when requested', async ({ request }) => {
     for(const option of inclExclOptions) {
+        // request data excluding one option
         const result = await request.get('./api/', { params: { 'exc': option } });
         expect(result.ok()).toBeTruthy();
+        // check result for that option
         expect(await result.json()).not.toHaveProperty(option);
     }
     
